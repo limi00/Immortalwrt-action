bec4b51679057ea7ad1ec24e946dc8a0d7ce92ab
limi00 <75186809+limi00@users.noreply.github.com>
2022-05-29 14:18:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,7 +24,7 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> "feeds.conf.default"
 
 # # Define My Package
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
@@ -39,4 +39,4 @@
 # svn co https://github.com/cloudysky9/openwrt-packages/trunk/app-store-ui package/app-store-ui
 # svn co https://github.com/cloudysky9/openwrt-packages/trunk/luci-app-store package/luci-app-store
 
-# rm -rf ./package/lean/luci-theme-argon && git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
+rm -rf ./package/lean/luci-theme-argon && git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
