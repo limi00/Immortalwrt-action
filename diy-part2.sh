545b6e92227b6c30b7fa827a19ec9de69a84adc1
limi00 <75186809+limi00@users.noreply.github.com>
2022-06-26 07:45:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,11 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/ImmortalWrt/XiaoMi_R3G/g' package/base-files/files/bin/config_generate
 
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 增加 SSID 2.5G
+sed -i '/channel="11"/a\\t\tssid="Xiaomi_DC08"' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 增加 SSID 5.0G
+sed -i '/channel="36"/a\\t\t\tssid="Xiaomi_DC08_5G"' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 修改默认 SSID
-sed -i 's/OpenWrt/${Xiaomi_DC08}/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
